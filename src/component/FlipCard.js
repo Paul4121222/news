f3054c49e850c48301c6342a98e03c243d8936d6
paul <scs55647875@gmail.com>
2022-02-06 21:10:51 +0800

    add card   href

--- src/component/FlipCard.js
+++ src/component/FlipCard.js
@@ -7,74 +7,75 @@ const FlipCard = (props)=>{
             <div className="card-row1">
                
                     <div className="clip-card-container">
-                        <div className="clip-card-space">
-                                <a href={props.list[0].url} target="_blank" rel="noreferrer noopener">
+                        <a className="clip-card-space" href={props.list[0].url} target="_blank">
+                               
                                 <div className="card-front ">
                                     <img src={props.list[0].image} alt="新聞圖片" />
                                 </div>
                                 <div className="card-back">
                                     <h2>{props.list[0].title}</h2>
                                 </div>
-                                </a>
-                        </div>
+                               
+                        </a>
                     </div>
                
 
                 <div className="clip-card-container">
                     
-                    <div className="clip-card-space">
-                            <a href={props.list[1].url} target="_blank" rel="noreferrer noopener">
+                    <a className="clip-card-space"  href={props.list[1].url} target="_blank">
+                           
                             <div className="card-front ">
                                 <img src={props.list[1].image} alt="新聞圖片" />
                             </div>
                             <div className="card-back">
                                 <h2>{props.list[1].title}</h2>
                             </div>
-                            </a>
-                    </div>
+                           
+                    </a>
                 </div>
 
                 <div className="clip-card-container">
-                    <div className="clip-card-space">
-                            <a href={props.list[2].url} target="_blank" rel="noreferrer noopener">
+                    <a className="clip-card-space"   href={props.list[2].url} target="_blank">
+                           
                             <div className="card-front ">
                                 <img src={props.list[2].image} alt="新聞圖片" />
                             </div>
                             <div className="card-back">
                                 <h2>{props.list[2].title}</h2>
                             </div>
-                            </a>
-                    </div>
+                          
+                    </a>
                 </div>
             </div>
 
             <div className="card-row2">
                 <div className="clip-card-container">
                     
-                    <div className="clip-card-space">
-                            <a href={props.list[3].url} target="_blank" rel="noreferrer noopener">
+                    <a className="clip-card-space"   href={props.list[3].url} target="_blank" >
+                          
                             <div className="card-front ">
                                 <img src={props.list[3].image} alt="新聞圖片" />
                             </div>
                             <div className="card-back">
                                 <h2>{props.list[3].title}</h2>
                             </div>
-                            </a>
-                    </div>
+                           
+                    </a>
                 </div>
 
                 <div className="clip-card-container">
-                    <div className="clip-card-space">
-                            <a href={props.list[4].url} target="_blank" rel="noreferrer noopener">
+                    <a className="clip-card-space"   href={props.list[4].url} target="_blank">
+                            
                             <div className="card-front ">
                                 <img src={props.list[4].image} alt="新聞圖片" />
                             </div>
                             <div className="card-back">
                                 <h2>{props.list[4].title}</h2>
                             </div>
-                            </a>
-                    </div>
+                            
+                    </a>
                 </div>
+               
             </div>
         </div>
     )
