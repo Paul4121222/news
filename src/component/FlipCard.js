9823ec8dffaec2ad90a225e976a8db8886252c52
root <root@PaulChang-NB.localdomain>
2025-02-07 17:46:11 +0800

    add background

--- src/component/FlipCard.js
+++ src/component/FlipCard.js
@@ -1,101 +1,105 @@
-import React from 'react';
+import React from "react";
 import video from "../video/Local.mp4";
-const FlipCard = (props)=>{
-    return (
-        <div className="padding-x  flip-card">
-                <video  className='card-video' autoPlay muted loop>
-                    <source src={video} type="video/mp4"></source>
-                </video>
-            
-               
-                    <div className="clip-card-container">
-                        <a className="clip-card-space" href={props.list[0].url} target="_blank" rel="noreferrer">
-                               
-                                <div className="card-front ">
-                                    <img src={props.list[0].image} alt="新聞圖片" />
-                                </div>
-                                <div className="card-back">
-                                    <h2>{props.list[0].title}</h2>
-                                </div>
-                               
-                        </a>
-                    </div>
-               
+const FlipCard = (props) => {
+  return (
+    <div className="padding-x  flip-card">
+      <div className="clip-card-container">
+        <a
+          className="clip-card-space"
+          href={props.list[0].url}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="card-front ">
+            <img src={props.list[0].image} alt="新聞圖片" />
+          </div>
+          <div className="card-back">
+            <h2>{props.list[0].title}</h2>
+          </div>
+        </a>
+      </div>
 
-                <div className="clip-card-container">
-                    
-                    <a className="clip-card-space"  href={props.list[1].url} target="_blank" rel="noreferrer">
-                           
-                            <div className="card-front ">
-                                <img src={props.list[1].image} alt="新聞圖片" />
-                            </div>
-                            <div className="card-back">
-                                <h2>{props.list[1].title}</h2>
-                            </div>
-                           
-                    </a>
-                </div>
+      <div className="clip-card-container">
+        <a
+          className="clip-card-space"
+          href={props.list[1].url}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="card-front ">
+            <img src={props.list[1].image} alt="新聞圖片" />
+          </div>
+          <div className="card-back">
+            <h2>{props.list[1].title}</h2>
+          </div>
+        </a>
+      </div>
 
-                <div className="clip-card-container">
-                    <a className="clip-card-space"   href={props.list[2].url} target="_blank" rel="noreferrer">
-                           
-                            <div className="card-front ">
-                                <img src={props.list[2].image} alt="新聞圖片" />
-                            </div>
-                            <div className="card-back">
-                                <h2>{props.list[2].title}</h2>
-                            </div>
-                          
-                    </a>
-                </div>
-           
+      <div className="clip-card-container">
+        <a
+          className="clip-card-space"
+          href={props.list[2].url}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="card-front ">
+            <img src={props.list[2].image} alt="新聞圖片" />
+          </div>
+          <div className="card-back">
+            <h2>{props.list[2].title}</h2>
+          </div>
+        </a>
+      </div>
 
-            
-                <div className="clip-card-container">
-                    
-                    <a className="clip-card-space"   href={props.list[3].url} target="_blank" rel="noreferrer">
-                          
-                            <div className="card-front ">
-                                <img src={props.list[3].image} alt="新聞圖片" />
-                            </div>
-                            <div className="card-back">
-                                <h2>{props.list[3].title}</h2>
-                            </div>
-                           
-                    </a>
-                </div>
+      <div className="clip-card-container">
+        <a
+          className="clip-card-space"
+          href={props.list[3].url}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="card-front ">
+            <img src={props.list[3].image} alt="新聞圖片" />
+          </div>
+          <div className="card-back">
+            <h2>{props.list[3].title}</h2>
+          </div>
+        </a>
+      </div>
 
-                <div className="clip-card-container">
-                    <a className="clip-card-space"   href={props.list[4].url} target="_blank" rel="noreferrer">
-                            
-                            <div className="card-front ">
-                                <img src={props.list[4].image} alt="新聞圖片" />
-                            </div>
-                            <div className="card-back">
-                                <h2>{props.list[4].title}</h2>
-                            </div>
-                            
-                    </a>
-                </div>
+      <div className="clip-card-container">
+        <a
+          className="clip-card-space"
+          href={props.list[4].url}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="card-front ">
+            <img src={props.list[4].image} alt="新聞圖片" />
+          </div>
+          <div className="card-back">
+            <h2>{props.list[4].title}</h2>
+          </div>
+        </a>
+      </div>
 
-                <div className="clip-card-container">
-                    <a className="clip-card-space"   href={props.list[2].url} target="_blank" rel="noreferrer">
-                            
-                            <div className="card-front ">
-                                <img src={props.list[2].image} alt="新聞圖片" />
-                            </div>
-                            <div className="card-back">
-                                <h2>{props.list[2].title}</h2>
-                            </div>
-                            
-                    </a>
-                </div>
-
-                
-               
-          
-        </div>
-    )
-}
+      <div className="clip-card-container">
+        <a
+          className="clip-card-space"
+          href={props.list[2].url}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="card-front ">
+            <img src={props.list[2].image} alt="新聞圖片" />
+          </div>
+          <div className="card-back">
+            <h2>{props.list[2].title}</h2>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default FlipCard;
--- src/component/New.js
+++ src/component/New.js
@@ -1,49 +1,66 @@
-import React from 'react';
-import {mainPage} from '../action';
-import {connect} from 'react-redux';
-import Slider from './Slider';
-import Footer from './Footer';
-import FlipCard from './FlipCard';
-class New extends React.Component{
-    componentDidMount(){
-        this.props.mainPage();
-    }
-    render(){
-        if(this.props.item.length===0){
-            return <div>loading</div>
-        }
-        return (
-            <div>
-                <FlipCard 
-                    list={[
-                        this.props.item[9],
-                        this.props.item[5],
-                        this.props.item[6],
-                        this.props.item[7],
-                        this.props.item[8]
-                    ]}
-                />
-                <Slider 
-                    list={[
-                    this.props.item[0],
-                    this.props.item[1],
-                    this.props.item[2],
-                    this.props.item[3],
-                    this.props.item[4]
-                ]}
-                />
+import React from "react";
+import { mainPage } from "../action";
+import { connect } from "react-redux";
+import Slider from "./Slider";
+import Footer from "./Footer";
+import FlipCard from "./FlipCard";
+import backgroundImage from "../assets/background.jpg";
 
-                
-                <Footer   />
-
-            </div>
-        )
+class New extends React.Component {
+  componentDidMount() {
+    this.props.mainPage();
+  }
+  render() {
+    if (this.props.item.length === 0) {
+      return <div>loading</div>;
     }
+    return (
+      <div>
+        <div
+          style={{
+            height: "500px",
+          }}
+        >
+          <div
+            style={{
+              position: "fixed",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              backgroundImage: `url(${backgroundImage})`,
+              backgroundRepeat: "no-repeat",
+            }}
+          ></div>
+        </div>
+        <FlipCard
+          list={[
+            this.props.item[9],
+            this.props.item[5],
+            this.props.item[6],
+            this.props.item[7],
+            this.props.item[8],
+          ]}
+        />
+        <Slider
+          list={[
+            this.props.item[0],
+            this.props.item[1],
+            this.props.item[2],
+            this.props.item[3],
+            this.props.item[4],
+          ]}
+        />
+
+        <Footer />
+      </div>
+    );
+  }
 }
 
-const mapStateToProps=(state)=>{
-    return {
-        item:state.newTheme
-    }
-}
-export default connect(mapStateToProps,{mainPage})(New);
+const mapStateToProps = (state) => {
+  return {
+    item: state.newTheme,
+  };
+};
+export default connect(mapStateToProps, { mainPage })(New);
